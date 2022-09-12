6c4c9e8ee5b4fc4f5b5527200d2e5e047a6dd9c2
sdominguer <88257827+sdominguer@users.noreply.github.com>
2022-09-11 22:00:34 -0500

    Update red_neuronal.py

--- red_neuronal.py
+++ red_neuronal.py
@@ -1,38 +1,45 @@
-
 from tabnanny import verbose
+
 import tensorflow as tf
+
 import numpy as np
+
 from keras.models import Sequential
-from keras.layers.core import Dense
-from generar import *
+
+from keras.layers import Dense
+
+
 
 # Datos de verdad
+
 #[graduado: 0|1, cod_ciudad: 000-1111, edad: 0, 2]
-training_data = np.array([[5, 17, 27], [0, 1], [1, 0], [1, 1], 'float32']) # Estimulos               #   ENTRENAN RED
-target_data = np.array([[0.000006], [0], [1], [1], 'float32']) # Acciones
+
+training_data = np.array([[22, 3], [17, 1], [10, 1], [24, 4]], 'float32') # Estimulos               #   ENTRENAN RED
+
+target_data = np.array([[1], [0], [0], [1]] ,'float32') # Acciones
+
 
 n_entrada = len(training_data[0]) #2
+
 n_nodos = 16
+
 n_salida = 1
 
+
+
 model = Sequential()
+
 model.add(Dense(n_nodos, input_dim = n_entrada, activation = "relu"))
+
 model.add(Dense(n_salida, activation = 'sigmoid'))
-model.compile(
-    loss = "mean_squared_error",
-    optimizer = "adam", 
-    metrics = ['binary_accuracy']
-)
-model.fit(training_data, target_data, epochs = 1000, verbose = 0) # verbose = 0 es sin eco (no lo muestra en pantalla)
 
 
-#==============================================================================
 
-print('='*30)
-score = model.evaluate(training_data, target_data, verbose = 1)
-print (f'Modelo de medicion: {model.metrics_names[1]}')
-print(f' ... valor del modelo: {score[1] * 100}')
-model.summary()
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['binary_accuracy'])
+
+model.fit(training_data, target_data, epochs = 1000, verbose = 1) # verbose = 0 es sin eco (no lo muestra en pantalla)
+
+
 
 #==============================================================================
 
